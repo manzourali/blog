5d0f572e061415540dc8d6953426b43842f0b859
Ali Manzour <manzour_ali@yahoo.com>
2025-01-20 14:16:03 +0330

    fix: remove blog from url

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,14 +1,8 @@
 const swconf = {
   
-<<<<<<< HEAD
-    cacheName: 'chirpy-1737368936',resources: [
+    cacheName: 'chirpy-1737369901',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
-=======
-    cacheName: 'chirpy-1737363747',resources: [
-      '/blog/assets/css/jekyll-theme-chirpy.css',
-      '/blog/',
->>>>>>> parent of e26ba98 (fix: revert last 2 commits changes)
       
         '/categories/',
       
