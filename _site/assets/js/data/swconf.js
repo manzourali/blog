f6255cc191828fbf4ace6c27f47992c5a0008f21
Ali Manzour <manzour_ali@yahoo.com>
2025-01-20 12:27:25 +0330

    fix: update env to production

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737362031',resources: [
+    cacheName: 'chirpy-1737363304',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
